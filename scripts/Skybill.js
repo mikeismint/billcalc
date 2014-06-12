be7907a02a24b40f5a2f98933fdab96575f64a42
Mike Wilson <mikeismint@gmail.com>
2014-06-12 15:29:01 +0100

    Added properties to Skybill class added parseDate helper function added dayDiff helper function

--- scripts/Skybill.js
+++ scripts/Skybill.js
@@ -1,3 +1,38 @@
 var Skybill = {
+
+  /********************* PROPERTIES *********************/
+  TV : {
+    cost: '',
+    inDate: '',
+    name: 'TV Bundle Price',
+  },
+
+  BB : {
+    cost: '',
+    inDate: '',
+    name: 'Sky Talk & Broadband Price',
+  },
+
+  /********************* HELPER FUNCTIONS *********************/
+  // TODO - Make Private if possible
+
+  parseDate : function(dateStr) {
+    /* Precondition: dateIn is a string in the format dd/mm/yy or dd/mm/yyyy
+     * Postcondition: returns JavaScript Date instance */
+
+    var dateParts = dateStr.split(/[/-]/);
+    //new Date(year, month, day)
+    return new Date(dateParts[2], dateParts[1]-1, dateParts[0]);
+  },
+
+  dayDiff : function(first, second) {
+    /* Precondition: Both parameters are Date instances
+     * Postcondition: returns number of days between the dates as an integer */
+
+    var msInDay = 1000*60*60*24;  // Milliseconds in a day (24 hours)
+    var diff = (second - first);  // Diference in Milliseconds
+    return Math.floor(diff/msInDay);
+  },
+
 }
 
